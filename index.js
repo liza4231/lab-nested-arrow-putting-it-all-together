661d46095c9d38bcd3417823e929865418b08918
Elizabeth salazar <liza4231@gmail.com>
2025-11-05 18:13:36 -0800

    1st attempt

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 
+  const userInfo = {
+  "username": "user1",
+  "password": "password123"
+}
 
+function createLoginTracker(userInfo) {
+  let attemptCount = 0
+
+    const checkPassword = (passwordAttempt) => {
+      attemptCount = attemptCount + 1;
+
+
+      if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+        return (`Login successful`);
+      } else if (passwordAttempt != userInfo.password && attemptCount <=3) {
+        return (`Attempt ${attemptCount}: Login failed`);
+      } else {
+        return ("Account locked due to too many failed login attempts")
+      }
+    }
+    return (attempt) => checkPassword(attempt)
+} 
+let user1Login = createLoginTracker(userInfo);
+user1Login()
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
